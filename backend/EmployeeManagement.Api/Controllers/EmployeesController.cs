ff006ddd633939add98301c41f1b961a905accec
Yuto <xmilkyx0326@gmail.com>
2025-07-23 23:51:42 +0900

    テスト

---
+++ backend/EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,73 @@
+using Backend.Enums;
+using Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        // DB作成までダミーデータ使用
+        private static List<Employee> _employees = new()
+        {
+            new Employee
+            {
+                Id = 1,
+                FullName = "TestEmpoyee1",
+                DateOfBirth = new DateTime(2000, 1, 1),
+                Gender = Gender.男性,
+                Address = "dummy1-1-1",
+                JoinDate = new DateTime(2020, 6, 1),
+                VacationRemaining = 5,
+                CurrentWorkplace = "workplace1",
+                Status = Status.Active
+            },
+
+            new Employee
+            {
+                Id = 2,
+                FullName = "TestEmpoyee2",
+                DateOfBirth = new DateTime(2001, 2, 2),
+                Gender = Gender.女性,
+                Address = "dummy2-2-2",
+                JoinDate = new DateTime(2021, 7, 1),
+                VacationRemaining = 10,
+                CurrentWorkplace = "workplace2",
+                Status = Status.Active
+            },
+
+            new Employee
+            {
+                Id = 3,
+                FullName = "TestEmpoyee3",
+                DateOfBirth = new DateTime(2003, 3, 3),
+                Gender = Gender.その他,
+                Address = "dummy3-3-3",
+                JoinDate = new DateTime(2020, 6, 1),
+                VacationRemaining = 15,
+                CurrentWorkplace = "workplace3",
+                Status = Status.Retired
+            }
+        };
+
+        // api/employee
+        [HttpGet]
+        [Authorize(Roles = "General")]
+        public ActionResult<Employee> GetMyEmployeeInfo()
+        {
+            var userIdClaim = User.FindFirst("userId")?.Value;
+
+            if (string.IsNullOrEmpty(userIdClaim) || !int.TryParse(userIdClaim, out int userId))
+                return Unauthorized("ユーザーIDが無効です");
+
+            var employee = _employees.FirstOrDefault(e => e.Id == userId);
+
+            if (employee == null)
+                return NotFound("該当する従業員情報が見つかりません");
+
+            return Ok(employee);
+        }
+    }
+}
